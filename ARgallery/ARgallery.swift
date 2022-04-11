8a9657d88682b03548d7a147626ad74ad749af8f
Michal Simik <michal.simik@gmail.com>
2022-04-11 23:24:27 +0200

    update: add crashlytics

--- ARgallery/ARgallery.swift
+++ ARgallery/ARgallery.swift
@@ -7,9 +7,15 @@
 
 import PartialSheet
 import SwiftUI
+import Firebase
 
 @main
 struct ModelPicker: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             MainContentView()
